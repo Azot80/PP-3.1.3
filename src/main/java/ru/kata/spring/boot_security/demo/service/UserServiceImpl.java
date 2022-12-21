e5cd52349f40495a60aaf8eb4c89abd2aaf9d95c
Алексей Зотеев <lione@inbox.ru>
2022-12-21 18:03:20 +0300

    Исправлены замечания

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
@@ -21,11 +22,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.getById(id);
     }
     @Override
+    @Transactional
     public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
     @Override
+    @Transactional
     public void removeUserById(Long id) {
         userRepository.deleteById(id);
     }
